50936404733daeabff19abb190955a53168d9e21
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-01-10 21:47:36 -0600

    Update main.py

--- Runner/main.py
+++ Runner/main.py
@@ -1,1 +1,21 @@
 import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+testSurface = pygame.Surface((100,200))
+testSurface.fill('Red')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        screen.blit(testSurface,(200,100))
+
+        pygame.display.update()
+        clock.tick(60)
