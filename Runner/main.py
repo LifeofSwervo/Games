fc1fe9809983397b830695ec8950988b135cb5c4
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-01-11 22:37:56 -0600

    Update main.py

--- Runner/main.py
+++ Runner/main.py
@@ -5,17 +5,44 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+testFont = pygame.font.Font('Runner/font/Pixeltype.ttf', 50)
 
-testSurface = pygame.Surface((100,200))
-testSurface.fill('Red')
+#Background (and text)
+skySurface = pygame.image.load('Runner/graphics/Sky.png').convert()
+groundSurface = pygame.image.load('Runner/graphics/ground.png').convert()
+textSurface = testFont.render('My Game', False, 'Green').convert()
 
+#Player
+playerSurface = pygame.image.load('Runner/graphics/Player/player_walk_1.png').convert_alpha()
+playerRect = playerSurface.get_rect(midbottom =(80,300))
+
+#Enemies
+
+    #Snail
+snailSurface = pygame.image.load('Runner/graphics/snail/snail1.png').convert_alpha()
+snailRect = snailSurface.get_rect(midbottom = (600,300))
+snailXPos = 600
+
+#Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-        screen.blit(testSurface,(200,100))
+    #Background
+    screen.blit(skySurface,(0,0))
+    screen.blit(groundSurface,(0,300))
+    screen.blit(textSurface,(300,50))
+    #Enemies
 
-        pygame.display.update()
-        clock.tick(60)
+        #Snail
+    snailRect.right  -= 5
+    if snailRect.left < -50: snailRect.left = 850
+    screen.blit(snailSurface,snailRect)
+
+    #Player
+    screen.blit(playerSurface,playerRect)
+
+    pygame.display.update()
+    clock.tick(60)
