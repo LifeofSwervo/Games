1532514b1e4f3c2a53d53bdb31b3497db44ef0a5
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-01-14 01:24:35 -0600

    Update

---
+++ Pong/pong+.py
@@ -0,0 +1,38 @@
+import pygame
+
+#Setup
+pygame.init()
+screenWidth = 800
+screenHeight = 400
+screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption('Pong +')
+clock = pygame.time.Clock()
+
+#Game Rectangles
+ball = pygame.Rect(screenWidth/2 - 15,screenHeight/2 - 15,30,30)
+player = pygame.Rect(screenWidth - 20, screenHeight/2 - 70,10,140)
+opponent = pygame.Rect(10, screenHeight/2 - 70, 10, 140)
+
+#Color
+bgColor = pygame.Color('grey12')
+lightGrey = (200,200,200)
+
+#Game Loop
+while True:
+    #Input Handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    #Visuals
+    screen.fill(bgColor)
+    pygame.draw.rect(screen,lightGrey,player)
+    pygame.draw.rect(screen,lightGrey,opponent)
+    pygame.draw.ellipse(screen,lightGrey,ball)
+
+
+
+
+    pygame.display.update()
+    clock.tick(60)
