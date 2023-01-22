d2b3d0a518eeb5f0aec98dcd38d5da3ede81cce2
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-01-21 23:20:11 -0600

    Update main.py

--- Ninja Runner/main.py
+++ Ninja Runner/main.py
@@ -3,16 +3,99 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('Assets/Player/Run/NinjaRun0.png')
-        self.rect = self.image.get_rect(midbottom = (80, 310))
+        playerRun1 = pygame.image.load('Assets/Player/Run/NinjaRun0.png')
+        playerRun2 = pygame.image.load('Assets/Player/Run/NinjaRun1.png')
+        playerRun3 = pygame.image.load('Assets/Player/Run/NinjaRun2.png')
+        playerRun4 = pygame.image.load('Assets/Player/Run/NinjaRun3.png')
+        playerRun5 = pygame.image.load('Assets/Player/Run/NinjaRun4.png')
+        playerRun6 = pygame.image.load('Assets/Player/Run/NinjaRun5.png')
+        playerRun7 = pygame.image.load('Assets/Player/Run/NinjaRun6.png')
+        playerRun8 = pygame.image.load('Assets/Player/Run/NinjaRun7.png')
+        self.playerRun = [playerRun1, playerRun2, playerRun3, playerRun4, playerRun5, playerRun6, playerRun7, playerRun8]
+        self.playerRunIndex = 0
+        self.running = True
 
+            #Jump
+        playerJump1 = pygame.image.load('Assets/Player/Jump/Jump0.png')
+        playerJump2 = pygame.image.load('Assets/Player/Jump/Jump1.png')
+        self.playerJump = [playerJump1, playerJump2]
+        self.playerJumpIndex = 0
+        self.jumping = False
 
-def golemAnimation():
-    global golemSurface, golemIdleIndex
+            #Fall
+        playerFall1 = pygame.image.load('Assets/Player/Jump/Fall0.png')
+        playerFall2 = pygame.image.load('Assets/Player/Jump/Fall1.png')
+        self.playerFall = [playerFall1, playerFall2]
+        self.playerFallIndex = 0
+        self.falling = False
+
     
-    golemIdleIndex += 0.2
-    if golemIdleIndex >= len(golemIdle): golemIdleIndex = 0
-    golemSurface = golemIdle[int(golemIdleIndex)]
+
+
+
+        self.image = self.playerRun[self.playerRunIndex]
+        self.rect = self.image.get_rect(midbottom = (190, 280))
+        self.gravity = 0
+
+    def playerInput(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 280:
+            self.jumping = True
+            self.gravity = -20
+            
+
+    def applyGravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 280: 
+            self.rect.bottom = 280
+            self.gravity = -1
+            self.jumping = False
+            self.falling = False
+    
+    def animationState(self):
+        if self.gravity > 0:
+            self.playerFallIndex += 0.2
+            if self.playerFallIndex >= len(self.playerFall): self.playerFallIndex = 0
+            self.image = self.playerFall[int(self.playerFallIndex)]
+        
+        elif self.jumping:
+            self.playerJumpIndex += 0.2
+            if self.playerJumpIndex >= len(self.playerJump): self.playerJumpIndex = 0
+            self.image = self.playerJump[int(self.playerJumpIndex)]
+            if self.gravity >= 0:
+                self.falling = True
+                self.jumping = False
+
+        elif self.falling:
+            if self.rect.bottom >= 280 and not self.jumping:
+                self.falling = False
+                self.running = True
+                self.image = self.playerRun[int(self.playerRunIndex)]
+            else:   
+                self.playerFallIndex += 0.2
+                if self.playerFallIndex >= len(self.playerFall): self.playerFall = 0
+                self.image = self.playerFall[int(self.playerFallIndex)]
+        
+        if self.running:
+            if self.rect.bottom >= 280:
+                self.jumping = True
+                self.running = False
+
+        else:
+            self.running = True
+            self.falling = False
+            self.playerRunIndex += 0.2
+            if self.playerRunIndex >= len(self.playerRun): self.playerRunIndex = 0
+            self.image = self.playerRun[int(self.playerRunIndex)]
+
+    def update(self):
+        self.playerInput()
+        self.applyGravity()
+        self.animationState()
+
+    
+
 
 def playerAnimation ():
     global playerSurface, playerRunIndex, playerJumpIndex, playerFallIndex, jumping, falling
@@ -38,7 +121,7 @@ def playerAnimation ():
             falling = False
 
     else:
-        playerRunIndex += 0.3
+        playerRunIndex += 0.2
         if playerRunIndex >= len(playerRun): playerRunIndex = 0
         playerSurface = playerRun[int(playerRunIndex)]
 
@@ -50,6 +133,9 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Ninja Run')
 clock = pygame.time.Clock()
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 
 #Background
 groundSurface = pygame.image.load('Assets/World/StraightLand.png')
@@ -88,19 +174,9 @@ PlayerRect = playerSurface.get_rect(midbottom = (80, 310))
 playerGravity = 0
 
 #Enemies
-golemIdle0 = pygame.image.load('Assets/Enemies/Idle/golemIdle0.png')
-golemIdle1 = pygame.image.load('Assets/Enemies/Idle/golemIdle1.png')
-golemIdle2 = pygame.image.load('Assets/Enemies/Idle/golemIdle2.png')
-golemIdle3 = pygame.image.load('Assets/Enemies/Idle/golemIdle3.png')
-golemIdle4 = pygame.image.load('Assets/Enemies/Idle/golemIdle4.png')
-golemIdle5 = pygame.image.load('Assets/Enemies/Idle/golemIdle5.png')
-golemIdle6 = pygame.image.load('Assets/Enemies/Idle/golemIdle6.png')
-golemIdle7 = pygame.image.load('Assets/Enemies/Idle/golemIdle7.png')
-golemIdle = [golemIdle0, golemIdle1, golemIdle2, golemIdle3, golemIdle4, golemIdle5, golemIdle6, golemIdle7]
-golemIdleIndex = 0
 
 
-golemSurface = golemIdle[golemIdleIndex]
+golemSurface = pygame.image.load('Assets/Enemies/Idle/golemIdle0.png')
 golemRect = golemSurface.get_rect(midbottom = (620, 270))
 
 #Game Loop
@@ -119,7 +195,7 @@ while True:
     screen.blit(skySurface, (0,0))
     screen.blit(groundSurface, (0,270))
     
-
+  
     #Player    
     playerGravity += 1
     PlayerRect.y += playerGravity 
@@ -136,9 +212,10 @@ while True:
 
     playerAnimation() 
     screen.blit(playerSurface, PlayerRect)
+    player.draw(screen)
+    player.update()
 
     #Golem
-    golemAnimation()
     screen.blit(golemSurface, golemRect)
 
 
