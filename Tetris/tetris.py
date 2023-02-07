5ee2d3ea8625f8e59f133f9435abbdeb7bf681c0
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-02-06 20:12:00 -0600

    Tetromino Setup

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,12 +1,23 @@
 from settings import *
 import math
+from tetromino import Tetromino
 
 class Tetris:
     def __init__(self, app):
         self.app = app
+        self.spriteGroup = pg.sprite.Group()
+        self.tetroimino = Tetromino(self)
     
+    def drawGrid(self):
+        for x in range(FIELD_W):
+            for y in range(FIELD_H):
+                pg.draw.rect(self.app.screen, 'black',
+                             (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def update(self):
-        pass
+        self.tetroimino.update()
+        self.spriteGroup.update()
 
     def draw(self):
-        pass
+        self.drawGrid()
+        self.spriteGroup.draw(self.app.screen)
