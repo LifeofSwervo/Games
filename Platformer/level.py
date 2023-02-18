99d5ca1e1acd4dc5384ace723e154b11105e8ff5
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-02-17 23:13:43 -0600

    Update level.py

--- Platformer/level.py
+++ Platformer/level.py
@@ -4,4 +4,10 @@ from tiles import Tile
 class Level:
     def __init__(self, levelData, surface):
         self.displaySurface = surface
-        self.levelData = levelData
+        self.setupLevel(levelData)
+
+    def setupLevel(self, layout):
+        self.tiles = pygame.sprite.Group()
+
+    def run(self):
+        pass
