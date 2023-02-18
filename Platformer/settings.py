3d2dee222062f5ecc665e64e67beea1ec22713ec
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-02-17 22:08:47 -0600

    1

---
+++ Platformer/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screenWidth = 1200
+screenHeight = len(level_map) * tile_size
