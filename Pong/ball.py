5f731de28c0b67b4da219d44ae93879573c0e852
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-03-23 05:24:37 -0500

    Ball Collisions

--- Pong/ball.py
+++ Pong/ball.py
@@ -13,21 +13,22 @@ class Ball(pg.sprite.Sprite):
         self.rect = pg.Rect(width / 2, height / 2, self.ballSize, self.ballSize)
 
         # Ball Movement
-        self.ballSpeed_x = 7 * random.choice((1, -1))
-        self.ballSpeed_y = 7 * random.choice((1, -1))
+        #self.ballSpeed_x = 7 * random.choice((1, -1))
+        #self.ballSpeed_y = 7 * random.choice((1, -1))
 
 
-    def ballAnimation(self):
-        self.rect.x += self.ballSpeed_x # Connects X of ball and Speed of ball.
-        self.rect.y += self.ballSpeed_y # Connects Y of ball and Speed of ball. 
+    #def ballAnimation(self):
+        #self.rect.x += self.ballSpeed_x # Connects X of ball and Speed of ball.
+        #self.rect.y += self.ballSpeed_y # Connects Y of ball and Speed of ball. 
 
         # Screen Constraints
-        if self.rect.top <= 0 or self.rect.bottom >= height: # Top of Screen Constraint & Bottom of Screen Constraint
-            self.ballSpeed_y *= -1                           
-        if self.rect.left <= 0 or self.rect.right >= width:  # Left of Screen Constraint & Right of Screen Constraint
-            self.ballSpeed_x *= -1                           
+        #if self.rect.top <= 0 or self.rect.bottom >= height: # Top of Screen Constraint & Bottom of Screen Constraint
+            #self.ballSpeed_y *= -1                           
+        #if self.rect.left <= 0 or self.rect.right >= width:  # Left of Screen Constraint & Right of Screen Constraint
+            #self.ballSpeed_x *= -1                           
 
 
     def update(self):
-        self.ballAnimation()
+        #self.ballAnimation()
+        pass
 
--- Pong/level.py
+++ Pong/level.py
@@ -3,12 +3,35 @@ from settings import *
 from player import Player
 from enemy import Enemy
 from ball import Ball
+import random
 
 class Level:
     def __init__(self, surface):
         # level setup
         self.displaySurface = surface
         self.setupLevel() 
+
+        #Ball Start
+            # Ball Trajectory set to random
+        self.ballSpeed_x = 7 * random.choice((1, -1)) 
+        self.ballSpeed_y = 7 * random.choice((1, -1))
+
+            # Ball Sprite
+        self.ballSprite = Ball()
+        self.ball.add(self.ballSprite)
+
+    def ballMovement(self):
+        # Ball Movement
+        self.ballSprite.rect.x += self.ballSpeed_x
+        self.ballSprite.rect.y += self.ballSpeed_y
+
+        # Screen Constraint
+        if self.ballSprite.rect.top <= 0 or self.ballSprite.rect.bottom >= height: # Top of Screen Constraint & Bottom of Screen Constraint
+            self.ballSpeed_y *= -1
+        if self.ballSprite.rect.left <= 0 or self.ballSprite.rect.right >= width: # Left of Screen Constraint & Right of Screen Constraint
+            self.ballSpeed_x *= -1
+
+
 
     def setupLevel(self):
         self.player = pg.sprite.GroupSingle()
@@ -24,15 +47,15 @@ class Level:
 
         ballSprite = Ball()
         self.ball.add(ballSprite)
+     
 
-        # Ball Setup 
-
-    
     def run(self):
         self.player.update()
         self.player.draw(self.displaySurface)
 
         self.enemy.draw(self.displaySurface)
         
-        self.ball.update()
+        #self.ball.update()
         self.ball.draw(self.displaySurface)
+        self.ballMovement()
+        
