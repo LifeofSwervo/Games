7a8f4459413e7c127e7a7a7d88675ac7afd50a1c
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-04-10 02:27:09 -0500

    Update index.js

--- Browser Game/'Canvas' JS Game/index.js
+++ Browser Game/'Canvas' JS Game/index.js
@@ -44,25 +44,67 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+// Player Coordinates
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
-player.draw()
+
 
 // Projectiles 
-
-
 const projectiles = []
+
+// Enemies
+const enemies = []
+function spawnEnemies() {
+    setInterval(() => {
+        const x = 100
+        const y = 100
+        const radius = 30
+        const color = 'green'
+        const velocity = {x: 1, y: 1}
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+        console.log(enemies)
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height) // Clear Projectile Trail
+    player.draw() // Draw Player after clearing Projectiles
     projectiles.forEach(projectile => {
         projectile.update()
     })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })
 }
 
+// Click Event Listener
 addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
     const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
@@ -72,3 +114,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
