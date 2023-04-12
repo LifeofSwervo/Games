0a2003e6177b7109715f077e0a2a81e5c2120c2c
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-04-11 20:49:03 -0500

    Update index.js

--- Browser Games/'Canvas' JS Game/index.js
+++ Browser Games/'Canvas' JS Game/index.js
@@ -71,7 +71,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 
 
 // Projectiles 
@@ -92,7 +92,7 @@ function spawnEnemies() {
             x = Math.random() * canvas.width // If random value is less than 0.5 -> 0 - radius (30) is X otherwise ScreenWidth + radius
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle = Math.atan2(canvas.height / 2 - y,canvas.width / 2 - x)
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
         enemies.push(new Enemy(x, y, radius, color, velocity))
@@ -100,24 +100,52 @@ function spawnEnemies() {
     }, 1000)
 }
 
+let animationId
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height) // Clear Projectile Trail
+    animationId = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height) // Clear Projectile Trail
     player.draw() // Draw Player after clearing Projectiles
-    projectiles.forEach(projectile => {
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+
+        // Projectile Constraint
+        if (projectile.x + projectile.radius < 0 ||
+             projectile.x - projectile.radius > canvas.width ||
+             projectile.y + projectile.radius < 0 ||
+             projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
 
     enemies.forEach((enemy, index) => {
         enemy.update()
 
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        // End Game
+        //if (dist - enemy.radius - player.radius < 1) {
+            //cancelAnimationFrame(animationId)           // Pauses animations after collision. 
+        //}
+
+
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
             
-
-            if (dist - enemy.radius - projectile.radius < 1) {
-                enemies.splice(index, 1)
-                projectiles.splice(projectileIndex, 1)
+            // Projectile & Object Collision
+            if (dist - enemy.radius - projectile.radius < 1) { // Removes white flash upon killing enemy. (Enemy is nested in array)
+                
+                if (enemy.radius > 10) {
+                    enemy.radius -= 10
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
+                
             }
         })
     })
@@ -126,9 +154,9 @@ function animate() {
 // Click Event Listener
 addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
-    const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
+    const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5}
     projectiles.push(
-        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
     )
 })
 
