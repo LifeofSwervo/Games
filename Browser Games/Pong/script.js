9b9fbab171bf6c48dc8575062bf9772d825b7497
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-04-17 17:03:49 -0500

    Update script.js

--- Browser Games/Pong/script.js
+++ Browser Games/Pong/script.js
@@ -13,6 +13,23 @@ const playerPaddle = new Paddle({position: {x: canvas.width / 25, y: 0},})
 const enemyPaddle = new Enemy({position: {x: canvas.width / 1.07, y: 0},})
 console.log(canvas.width)
 
+// Player Movement 
+addEventListener('keydown', (event) => {
+    switch (event.key) { // Focus on [s] key
+        case 's':
+            // Go Down
+        break
+        case 'w':
+            // Go Up
+        break
+        case 'a':
+            // Go Left
+        break
+        case 'd':
+            // Go Right    
+    }   
+    
+})
 
 playerPaddle.draw()
 enemyPaddle.draw()
