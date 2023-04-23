c8d71cbc260f4583626f2787dda3dcd8a500a219
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-04-23 10:54:35 -0500

    Collisions

--- Browser Games/Pong/script.js
+++ Browser Games/Pong/script.js
@@ -16,7 +16,62 @@ console.log(canvas.width)
     // Ball
 const ball = new Ball({position: {x: canvas.width / 2, y: canvas.height / 2}});
 
+// Collisions
+function collisions() {
+    // Ball & Player Collisions
+    const ballLeftSide = ball.position.x + ball.velocity.x
+    const ballBottomSide = ball.position.y + ball.height
+    const ballRightSide = ball.position.x + ball.width + ball.velocity.x
+    const ballTopSide = ball.position.y
+  
+    // Player Vars
+    const playerRightSide = playerPaddle.position.x + playerPaddle.width;
+    const playerTopSide = playerPaddle.position.y
+    const playerBottomSide = playerPaddle.position.y + playerPaddle.height
+    const playerLeftSide = playerPaddle.x 
 
+    // Enemy Vars
+    const enemyRightSide = enemyPaddle.position.x + enemyPaddle.width;
+    const enemyTopSide = enemyPaddle.position.y;
+    const enemyBottomSide = enemyPaddle.position.y + enemyPaddle.height;
+    const enemyLeftSide = enemyPaddle.x
+  
+    // Ball & Player Collisions
+    if ((ballBottomSide >= playerTopSide &&
+        ballTopSide <= playerBottomSide &&
+        ballLeftSide <= playerRightSide) || ballLeftSide <= 0) {
+            // Collision
+            ball.velocity.x = -ball.velocity.x
+            console.log('test')
+    }
+
+    // Ball & Enemy Paddle Collisions
+    if ((ballBottomSide >= enemyTopSide &&
+        ballTopSide <= enemyBottomSide &&
+        ballRightSide >= enemyLeftSide) || ballRightSide >= canvas.width) {
+            // Collisions
+            ball.velocity.x = -ball.velocity.x
+            console.log('test2')
+    };
+};
+
+function enemyAI() {
+    const enemySpeed = 3
+    if (ballBottomSide > enemyBottomSide) {
+        // Go Down
+        enemyPaddle.velocity.y = enemySpeed
+    }
+
+    if (ballTopSide < enemyTopSide) {
+        // Go Up
+        enemyPaddle.velocity.y = -enemySpeed
+    }
+
+    if (ball.position.y = enemyPaddle.position.y) {
+        // Remain Still
+        enemyPaddle.velocity.y = 0
+    }
+}
 
 
 // Player Movement 
@@ -44,30 +99,10 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
 
-    // Ball & Player Collisions
-    const ballLeftSide = ball.position.x + ball.velocity.x
-    const ballBottomSide = ball.position.y + ball.height
-    const ballRightSide = ball.position.x + ball.width + ball.velocity.x
-    const ballTopSide = ball.position.y
-
-    // Player Vars
-    const playerRightSide = playerPaddle.position.x + playerPaddle.width;
-    const playerTopSide = playerPaddle.position.y
-    const playerBottomSide = playerPaddle.position.y + playerPaddle.height
-    const playerLeftSide = playerPaddle.x 
-
-    // Ball & Player Collisions
-    if ((ballBottomSide >= playerTopSide &&
-        ballTopSide <= playerBottomSide &&
-        ballLeftSide <= playerRightSide) || ballLeftSide <= 0) {
-            // Collision
-        ball.velocity.x = -ball.velocity.x
-        console.log('test')
-    }
-
+    collisions()
+    
     playerPaddle.update()
     enemyPaddle.update()
-
     ball.update()
 }
 
