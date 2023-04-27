4efde22be272dc336501aa10bd8870daec9b54b5
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-04-26 20:34:03 -0500

    Movement 0.0.1

--- Browser Games/SpacePage/script.js
+++ Browser Games/SpacePage/script.js
@@ -1,6 +1,50 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+let speed = 5
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+
+}
+
+
+
+function movement() {
+    // Movement
+    addEventListener('keydown', ({ key }) => {
+        switch (key) {
+            case 'a':
+                keys.a.pressed = true
+            case 'd':
+                keys.d.pressed = true
+                break
+        }
+    })
+
+    // Stop Movement
+    addEventListener('keyup', ([ key ]) => {
+        switch (key) {
+            case 'a':
+                keys.a.pressed = false
+            case 'd':
+                keys.d.pressed = false
+                break
+        }
+    })
+}
+
+function movementInConstraint() {
+    if (keys.a.pressed && player.position.x >= 0) { // Constraint for left side of screen
+        player.velocity.x = -speed
+    }
+}
+
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
@@ -13,6 +57,11 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height) // Make Background Black
     player.update()
+
+    movementInConstraint()
 }
 
 animate()
+
+// Movement
+movement()
