a13e61b5518bee00511bdc62b518839170e1dab0
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-05-11 13:55:28 -0500

    Score and Reset (Work in Progress)

--- Browser Games/test/ball.js
+++ Browser Games/test/ball.js
@@ -6,11 +6,11 @@ class Ball {
         this.y = y;
         this.bounce = 0
 
-
+        const ballSpeed = 1.5
         // Allow ball to spawn going in random direction
-        const direction = { // Forces Math.random() to return either 1 or -1. (- or + choose directions)
-            x: Math.random() - 0.5 >= 0 ? -1 : 1, // If -0.5 to 0 returns -1, if 0 to 5 returns 1
-            y: Math.random() - 0.5 >= 0 ? -1 : 1 //  If -0.5 to 0 returns -1, if 0 to 5 returns 1
+        const direction = { // Forces Math.random() to return either ballSpeed or -ballSpeed. (- or + choose directions)
+            x: Math.random() - 0.5 >= 0 ? -ballSpeed : ballSpeed, // If -0.5 to 0 returns -ballSpeed, if 0 to 5 returns ballSpeed
+            y: Math.random() - 0.5 >= 0 ? -ballSpeed : ballSpeed //  If -0.5 to 0 returns -ballSpeed, if 0 to 5 returns ballSpeed
         }; 
 
         this.radius = 10;
@@ -26,16 +26,15 @@ class Ball {
     collisions() {
         // X coordinate constraint
         if (this.x + this.radius + this.velocity.x >= canvas.width || this.x + this.radius + this.velocity.x <= 0) {
-            this.velocity.x = -this.velocity.x
             this.bounce += 1
-            console.log(this.bounce)
+            //console.log(this.bounce)
         };
 
         // y Coordinate Constraint
         if (this.y + this.radius + this.velocity.y >= canvas.height || this.y + this.radius + this.velocity.y <= 0) {
             this.velocity.y = -this.velocity.y
             this.bounce += 1
-            console.log(this.bounce)
+            //console.log(this.bounce)
         };
     }
 
--- Browser Games/test/script.js
+++ Browser Games/test/script.js
@@ -3,10 +3,14 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const speed = 5
+const scoreElem = document.getElementById('scoreElem')
+const enemyScoreElem = document.getElementById('enemyScoreElem')
+
+const speed = 6
 const keys = {
     w: {
         pressed: false
@@ -15,6 +19,20 @@ const keys = {
         pressed: false
     }
 }
+
+// Center Line
+const centerLine = new CenterLine(canvas.width / 2, 0)
+
+// Player
+const player = new Player(canvas.width / 80, 100);
+let playerScore = 0
+
+// Enemy
+const enemy = new Enemy(canvas.width / 1.02, 100)
+let enemyScore = 0
+
+// Ball
+const ball = new Ball(canvas.width / 2, canvas.height / 2);
 
 function movementConstraint() {
     if (keys.w.pressed && player.y >= 0) {
@@ -64,30 +82,31 @@ function movement() {
 }
 
 function collisons() {
-    if ((ball.y + ball.radius >= player.y &&
+    if (ball.y + ball.radius >= player.y &&
         ball.x <= player.x + player.width &&
-        ball.y <= player.y + player.height) || ball.x <= 0) {
+        ball.y <= player.y + player.height) {
         ball.velocity.x = -ball.velocity.x
     }
 
-    if ((ball.y + ball.radius >= enemy.y &&
+    if (ball.y + ball.radius >= enemy.y &&
         ball.x + ball.radius >= enemy.x &&
-        ball.y <= enemy.y + enemy.height) || ball.x >= canvas.width) {
-            ball.velocity.x = -ball.velocity.x
-        }
+        ball.y <= enemy.y + enemy.height) {
+        ball.velocity.x = -ball.velocity.x
+    }
+
+    if (ball.x <= 0) {
+        ball.x = canvas.width / 2;
+        enemyScore += 1;
+        enemyScoreElem.innerHTML = enemyScore
+    };
+
+    if (ball.x >= canvas.width) {
+        ball.x = canvas.width / 2;
+        playerScore += 1;
+        scoreElem.innerHTML = playerScore;
+        
+    }
 }
-
-// Center Line
-const centerLine = new CenterLine(canvas.width / 2, 0)
-
-// Player
-const player = new Player(canvas.width / 35, 100);
-
-// Enemy
-const enemy = new Enemy(canvas.width / 1.07, 100)
-
-// Ball
-const ball = new Ball(canvas.width / 2, canvas.height / 2);
 
 function animate() {
     requestAnimationFrame(animate);
