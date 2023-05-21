dffe4ecfce72491c5b14476e95dc1f7220ba1edb
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-05-21 18:58:43 -0500

    Update script.js

--- Browser Games/'Canvas' JS Game/First-Canvas-Game_Remake/script.js
+++ Browser Games/'Canvas' JS Game/First-Canvas-Game_Remake/script.js
@@ -13,7 +13,26 @@ const x = canvas.width / 2;
 const y = canvas.height / 2;
 const player = new Player(x, y, 10, 'white');
 
+const projectiles = []
+
 const enemies = [];
+
+function shootProjectile() {
+    projectiles.forEach((projectile, index) => {
+        // Shoot
+        projectile.update()
+
+        // Screen Constraint
+        if (projectile.x + projectile.radius <= 0 ||
+            projectile.x - projectile.radius >= canvas.width ||
+            projectile.y + projectile.radius <= 0 ||
+            projectile.y - projectile.radius >= canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
+    })
+}
 
 // Enemy Spawn Function
 function spawnEnemies() {
@@ -39,27 +58,52 @@ function spawnEnemies() {
 let animationId
 let score = 0
 function animate() {
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
     c.fillStyle = 'rgba(0, 0, 0, 0.1)';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
+    shootProjectile();
 
     enemies.forEach((enemy) => {
         enemy.update()
 
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        // End Game
+        //if (dist - enemy.radius - player.radius < 1) {
+        //    cancelAnimationFrame(animationId)
+        //}
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            // Projectile & Object Collision
+            if (dist - enemy.radius - projectile.radius < 1) {
+
+                // Increase Score
+                score += 100;
+                scoreElem.innerHTML = score;
+                
+                // Particle Explosion
+            }
+        })
+
+
     })
 };
 
 // Shooting Projectile 
 addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height /2, event.clientX - canvas.width / 2)
+    const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5}
+    projectiles.push(
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
+    )
 })
 
 // Start Game
 startGameBtn.addEventListener('click', () => {
     animate();
     spawnEnemies();
-    console.log(enemies)
     modalElem.style.display = 'none';
 });
