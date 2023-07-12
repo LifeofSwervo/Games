74a0b45c15846c209f8167a1b25da0a8200754b4
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-12 02:48:56 -0500

    Update script.js

--- Browser Games/Snake/script.js
+++ Browser Games/Snake/script.js
@@ -71,4 +71,46 @@ let helpers = {
       CTX.closePath();
     }
   },
+  randHue() {
+    return ~~(Math.random() * 360);
+  },
+  hsl12rgb(hue, saturation, lightness) {
+    if (hue == undefined) {
+      return [0, 0, 0];
+    }
+    var chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
+    var huePrime = hue / 60;
+    var secondComponent = chroma * (1 - Math.abs((huePrime % 2) - 1));
+
+    huePrime = ~~huePrime;
+    var red;
+    var green;
+    var blue;
+
+    if (huePrime === 0) {
+      red = chroma;
+      green = secondComponent;
+      blue = 0;
+    } else if (huePrime === 1) {
+      red = secondComponent;
+      green = chroma;
+      blue = 0;
+    } else if (huePrime === 2) {
+      red = 0;
+      green = chroma;
+      blue = 0;
+    } else if (huePrime === 3) {
+      red = 0;
+      green = secondComponent;
+      blue = chroma;
+    } else if (huePrime === 4) {
+      red = secondComponent;
+      green = 0;
+      blue = chroma;
+    } else if (huePrime === 5) {
+      red = chroma;
+      green = 0;
+      blue = secondComponent;
+    }
+  }
 }  
