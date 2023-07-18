327898410a69543c75b779233584df40bcd2c779
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-18 02:39:47 -0500

    Initial variables

--- StarterGame/NinjaAttack/GameScene.swift
+++ StarterGame/NinjaAttack/GameScene.swift
@@ -2,6 +2,15 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+  let player = SKSpriteNode(imageNamed: "player")
   
+  var monstersDestroyed = 0
+  
+  override func didMove(to view: SKView) {
+    backgroundColor = SKColor.white
+    
+    player.position = CGPoint(x: size.width * 0.1, y: size.he  )
+    
+  }
 }
--- StarterGame/NinjaAttack/GameViewController.swift
+++ StarterGame/NinjaAttack/GameViewController.swift
@@ -7,6 +7,17 @@ class GameViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
+    
+    scene.scaleMode = .resizeFill
+    
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    
+    skView.presentScene(scene)
 
   }
   
