f89f2f5583e4d7fdcd6f642e311c019a3a1930bf
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-19 11:45:44 -0500

    Add Monster

--- StarterGame/NinjaAttack/GameScene.swift
+++ StarterGame/NinjaAttack/GameScene.swift
@@ -3,6 +3,8 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+  
+  // Create a player variable. Assign image
   let player = SKSpriteNode(imageNamed: "player")
   
   var monstersDestroyed = 0
@@ -10,7 +12,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.white
     
-    player.position = CGPoint(x: size.width * 0.1, y: size.he  )
+    // Player Position
+    player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
     
+    // Add player as child of the scene
+    addChild(player)
+  }
+  
+  func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+  func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+  }
+  
+  func addMonster() {
+    
+    // Create a monster variable
+    let monster = SKSpriteNode(imageNamed: "monster")
+    
+    // Randomize where the monster will spawn (along Y axis)
+    let actualY = random(min: monster.size.height / 2, max: size.height - monster.size.height / 2)
   }
 }
