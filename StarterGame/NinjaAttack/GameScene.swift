fbfca30b17f3698c2d78f29826fc89c1686480c2
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-19 15:14:57 -0500

    Vectors

--- StarterGame/NinjaAttack/GameScene.swift
+++ StarterGame/NinjaAttack/GameScene.swift
@@ -2,6 +2,40 @@
 
 import SpriteKit
 
+// Vectors
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+      func sqrt(a: CGFloat) -> CGFloat {
+  return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
+
+// Game Scene Class
 class GameScene: SKScene, SKPhysicsContactDelegate {
   
   // Create a player variable. Assign image
