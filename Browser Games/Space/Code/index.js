dbfb1a22eddf4ee6c6b84b24f83fc3af41c4ca6f
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-21 23:47:14 -0500

    Space Contribtions

--- Browser Games/Space/Code/index.js
+++ Browser Games/Space/Code/index.js
@@ -14,6 +14,19 @@ const enemies = [];
 // Particles
 particles = [];
 
+// Projectile
+projectiles = [];
+
+
+// Click event listeners
+addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+    const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5}
+    projectiles.push(
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
+    )
+})
+
 // Stars on Screen
 for (let i = 0; i < 100; i++) {
     particles.push(new Particle({position: {x: Math.random() * canvas.width, y: Math.random() * canvas.height}, 
@@ -22,6 +35,7 @@ for (let i = 0; i < 100; i++) {
     color: 'white'}))
 }
 
+// Spawns enemies at 1 second intervals
 function spawnEnemies() {
     setInterval(() => {
         const radius = Math.random() * (30 - 4) + 4 // Create radius as any value from 4 to 30
@@ -33,7 +47,7 @@ function spawnEnemies() {
             x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius // if random value is less than 0.5 -> 0 - radius(30) is x otherwise screenWidth + radius 
             y = Math.random() * canvas.height
         } else {
-            x = Math.randonm() * canvas.width //
+            x = 0 // Keeps enemies from spanwing along width (Locked to left and right)
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
         const color = `hsl(${Math.random() * 360}, 50%, 50%)`
@@ -69,7 +83,11 @@ function animate() {
     player.update()
 
     enemies.forEach((enemy, index) => {
-        enemy.update()
+        enemy.update()   
+    })
+
+    projectiles.forEach((projectile, index) => {
+        projectile.update()
     })
 }
 
