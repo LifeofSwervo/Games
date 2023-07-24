86401fb3dc084590e0e118c661149779f8b6aef2
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-24 18:59:21 -0500

    drawBricks function

--- Browser Games/Breakout/guide/index.js
+++ Browser Games/Breakout/guide/index.js
@@ -7,7 +7,23 @@ document.addEventListener("keyup", keyUpHandler, false);
 let leftPressed = false;
 let rightPressed = false;
 
+// Brick Field
+const brickRowCount = 3; 
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30
 
+
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0};
+    }
+}
 // Ball Variables
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -20,6 +36,22 @@ const paddleHeight = 10;
 const paddleWidth = 75;
 paddleX = (canvas.width - paddleWidth) / 2;
 
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function ballConstraint() {
     // Ball Constrainsts
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) { // x axis constraint
@@ -29,9 +61,13 @@ function ballConstraint() {
     if (y + dy < ballRadius) {
         dy = -dy
     } else if (y + dy > canvas.height - ballRadius) { // y axis constraint
+        if (x > paddleX && x < paddleX + paddleWidth) { // Paddle Collision
+            dy = -dy;
+        } else {
         alert("Game Over");
         document.location.reload();
         clearInterval(interval); // Chrome requirement
+        }
     }
 }
 
@@ -68,6 +104,9 @@ function drawPaddle() {
 }
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    drawBricks()
 
     drawPaddle()
     drawBall()
