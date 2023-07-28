878a2c05a1f9bc26a7a9196b88292db76d432610
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-27 20:52:33 -0500

    Player Movement and Key Registration

--- Browser Games/Breakout/Space Breakout/JS/index.js
+++ Browser Games/Breakout/Space Breakout/JS/index.js
@@ -6,14 +6,56 @@ canvas.height = window.innerHeight;
 
 const speed = 6;
 
+// Keys
+let rightPressed = false;
+let leftPressed = false;
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);
+
+// Key Functions
+function keyUpHandler(event) {
+    if (event.key === "Right" || event.key === "ArrowRight") {
+        rightPressed = false;
+    } else if (event.key === "Left" || event.key === "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+function keyDownHandler(event) {
+    if (event.key === "Right" || event.key === "ArrowRight") {
+        rightPressed = true;
+    } else if (event.key === "Left" || event.key === "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+
 // Player
 const paddleHeight = 10;
 const paddleWidth = 75;
-const paddle = new Paddle((canvas.width / 2) - (paddleWidth / 2), canvas.height - (paddleHeight * 2), paddleWidth, paddleHeight, "#9047FF");
+paddleX = (canvas.width - paddleWidth) / 2;
+const paddle = new Paddle(paddleX, canvas.height - (paddleHeight * 5), paddleWidth, paddleHeight, "#9047FF");
+
+
+
+    // Player Movement
+function playerMovement() {
+    
+}
 
 function draw() {
     c.clearRect(0, 0, canvas.width, canvas.height);
-    paddle.update()
+    paddle.update();
+    if (rightPressed) {
+        paddle.x += 7; // Paddle goes right
+        if (paddle.x + paddleWidth > canvas.width) {
+            paddle.x = canvas.width - paddleWidth;
+        }
+    } else if (leftPressed) {
+        paddle.x -= 7; // Paddle goes left
+        if (paddle.x <= 0) {
+            paddle.x = 0;
+        }
+    }
 }
 
-draw()
+const interval = setInterval(draw, 10)
