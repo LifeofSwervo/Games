37f071a70c1f6225244a54e3605c20745081e70c
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-08-19 23:52:03 -0500

    Logic

--- Browser Games/Conway's Game of LIfe/index.js
+++ Browser Games/Conway's Game of LIfe/index.js
@@ -13,6 +13,7 @@ function createField() {
         var tr = document.createElement('tr'); // Creates new table row for each iteration
         var tdElements = [];
         cells.push(new Array(size).fill(EMPTY)) // Initilizes each cell with the empty value
+        htmlElements.push(tdElements)
         table.appendChild(tr);
         for (var x = 0; x < size; x++) {
             var td = document.createElement('td'); // creates a new <td> (table cell) element for each iteration of the inner loop. The variable td holds a reference to this newly created cell.
@@ -21,3 +22,58 @@ function createField() {
         }
     }
 }
+
+function draw() {
+    for (var y = 0; y < size; y++) {
+        for (var x = 0; x < size; x++) {
+            htmlElements[y][x].setAttribute('class', 'cell' + (cells[y][x] == 1 ? 'filled' : 'empty'))
+        }
+    }
+}
+
+function countNeighbors(x, y) {
+    var count = 0;
+    for (dy = -1; dy <= 1; dy++) {
+        for (dx = -1; dx <= 1; dx++) {
+            var nx = (x + dx + size) % size, ny = (y + dy + size) % size;
+            count = count + cells[ny][nx]
+        }
+    }
+    return count - cells[y][x];
+}
+function newGeneration() {
+    var newCells = [];
+    for (var i = 0; i < size; i++) {
+        newCells.push(new Array(size).fill(EMPTY));
+    }
+    for (var y = 0; y < size; y++) {
+        for (var x = 0; x < size; x++) {
+            var neighbors = countNeighbors(x, y);
+            if (cells[y][x] == EMPTY && neighbors == 3) {
+                newCells[y][x] = ALIVE;
+            }
+            if (cells[y][x] == ALIVE && (neighbors == 2 || neighbors == 3)) {
+                newCells[y][x] = ALIVE;
+            }
+        }
+    }
+    cells = newCells;
+    draw();
+}
+function init() {
+    createField();
+    for (var i = 0; i < Math.floor(size * size * 0.3); i++) {
+        var x, y;
+        do {
+            x = Math.floor(Math.random() * size), y = Math.floor(Math.random() * size);
+            if (cells[y][x] == EMPTY) {
+                cells[y][x] = ALIVE;
+                break;
+            }
+        }   while(true); 
+    } 
+    draw();
+    setInterval(newGeneration, 100); 
+}
+init()
+ 
