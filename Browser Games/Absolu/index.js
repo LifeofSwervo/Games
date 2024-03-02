ac5d0c50249ab4c80e7d9455f18d12093d6dd4e6
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-03-02 12:41:33 -0600

    Player centered on screen

--- Browser Games/Absolu/index.js
+++ Browser Games/Absolu/index.js
@@ -6,8 +6,14 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+// Player info
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, 'white');
+
 function animate() {
   requestAnimationFrame(animate);
-  console.log("Hello World!");
   c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
 }
+
+animate();
