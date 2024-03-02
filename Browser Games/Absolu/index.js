336ae68f1d5c4237c867c92e45494d00d70c917e
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-03-02 16:51:21 -0600

    Player Centered Movement

--- Browser Games/Absolu/index.js
+++ Browser Games/Absolu/index.js
@@ -7,13 +7,56 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 // Player info
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, 'white');
+const player = new Player(0, 0, 30, 'white');
+
+let camera = {
+  x: 0,
+  y: 0,
+  width: canvas.width,
+  height: canvas.height,
+  update: function(player) {
+      // Keep the player in the center of the screen
+      this.x = player.x - this.width / 2;
+      this.y = player.y - this.height / 2;
+  }
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
+  camera.update(player);
   player.update();
+};
 
-}
+// Event listeners for player movement
+window.addEventListener('keydown', function(e) {
+  switch(e.key) {
+    case 'ArrowLeft':
+      player.moveLeft();
+      break;
+    case 'ArrowRight':
+      player.moveRight();
+      break;
+    case 'ArrowUp':
+      player.moveUp();
+      break;
+    case 'ArrowDown':
+      player.moveDown();
+      break;
+  }
+});
+
+window.addEventListener('keyup', function(e) {
+  switch(e.key) {
+    case 'ArrowLeft':
+    case 'ArrowRight':
+      player.stopX();
+      break;
+    case 'ArrowUp':
+    case 'ArrowDown':
+      player.stopY();
+      break;
+  }
+});
 
 animate();
--- Browser Games/Absolu/player.js
+++ Browser Games/Absolu/player.js
@@ -6,18 +6,47 @@ class Player
         this.y = y;
         this.radius = radius;
         this.color = color;
+        this.speed = 5;
+        this.dx = 0; // Change in x
+        this.dy = 0; // Change in y
     }
 
     draw() 
     {
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.arc(this.x - camera.x, this.y - camera.y, this.radius, 0, Math.PI * 2);
         c.fillStyle = this.color;
         c.fill();
     }
 
     update() 
     {
+        this.x += this.dx;
+        this.y += this.dy;
         this.draw();
+    }
+
+    moveLeft() {
+        this.dx = -this.speed;
+    }
+
+    moveRight() {
+        this.dx = this.speed;
+    }
+
+    moveUp() {
+        this.dy = -this.speed;
+    }
+
+    moveDown() {
+        this.dy = this.speed;
+    }
+
+    stopX() {
+        this.dx = 0;
+    }
+
+    stopY() {
+        this.dy = 0;
     }
 }
