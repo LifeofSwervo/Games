09bf9a1c17f6b83db2c13138f9a8449d62dc0425
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-09-17 19:48:56 -0500

    Examples import

---
+++ StarterGame/C++/Pong/MacOS/Examples/Examples/Examples/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Examples
+//
+//  Created by Paul Thomas on 9/17/24.
+//
+
+#include <iostream>
+#include <raylib.h>
+
+int main() {
+    using namespace std;
+    // insert code here...
+    cout << "Starting Game...";
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 800;
+    
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Examples");
+    
+    
+}
